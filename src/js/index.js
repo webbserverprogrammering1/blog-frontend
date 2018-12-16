f84bc38c1834d5d22b5509940a2cbe7829807cde
Unknown <johan.gudmundsson2012@gmail.com>
2018-12-16 16:17:39 +0100

    Add function to load blog posts

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+const loadBlogPosts = async () => {
+  try {
+    const response = await fetch('http://localhost:8000/posts');
+    if (response.status === 404) {
+      renderError('Kunde inte hitta några bloggposter!');
+    }
+    const json = await response.json();
+    renderBlogPosts(json);
+  } catch (e) {
+    renderError('Webbservern finns inte tillgänglig på angiven port!');
+  }
+};
