c9adfeb1d62f0bdbd28c327c6b3f8d2ed1fa1497
Unknown <johan.gudmundsson2012@gmail.com>
2018-12-16 16:18:22 +0100

    Add function to render blog posts

--- src/js/index.js
+++ src/js/index.js
@@ -13,4 +13,34 @@ const loadBlogPosts = async () => {
     renderError('Webbservern finns inte tillgänglig på angiven port!');
   }
 };
+
+const renderBlogPost = async event => {
+  event.preventDefault();
+  const postId = event.target.getAttribute('data-id');
+  const response = await fetch(`http://localhost:8000/posts/${postId}`);
+  if (response.status === 404) {
+    const errorEl = document.getElementsByClassName(`error${postId}`)[0];
+    errorEl.textContent = 'Kunde inte hitta post!';
+  }
+  const data = await response.json();
+  const post = data[0];
+  setPageTitle(post.title);
+
+  const html = `
+    <header>
+      <a class="back" href="#">&#x2190; Tillbaka</a>
+    </header>
+    <article>
+      <p>Skriven av: <span class="primary">${post.writtenBy}</span></p>
+      <p>${post.content}</p>
+    </article>
+    `;
+  root.innerHTML = html;
+  document.documentElement.scrollTop = 0;
+  document.getElementsByClassName('back')[0].addEventListener('click', () => {
+    setPageTitle();
+    loadBlogPosts();
+  });
+};
+
 loadBlogPosts();
